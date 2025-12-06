95b3531f80166d4b4f7f42ee784fc82fa9d12622
nadhifxfx <119722690+Nadhifxfx@users.noreply.github.com>
2025-12-06 19:15:50 +0700

    updt RAD

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,6 @@
 import Hero from '../components/Hero';
 import AboutGame from '../components/AboutGame';
 import AILearning from '../components/AILearning';
-import RADProcess from '../components/RADProcess';
 import Gallery from '../components/Gallery';
 import Contact from '../components/Contact';
 
@@ -11,7 +10,6 @@ export default function Home() {
       <Hero />
       <AboutGame />
       <AILearning />
-      <RADProcess />
       <Gallery />
       <Contact />
     </div>
